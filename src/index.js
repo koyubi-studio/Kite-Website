713164021c7086940a426b14710445814a65f196
koyubi-studio <hello@koyubi.studio>
2025-12-05 23:40:29 -0500

    Update index.js
    Adding form imports

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css"; // ★ これを追加
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);

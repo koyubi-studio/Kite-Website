07b53a8e2600b3d63b8954be53d1cc03b35e8872
koyubi-studio <hello@koyubi.studio>
2025-12-05 23:45:29 -0500

    Update index.js
    Updating form import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import App from "./App";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles.css"; // ★ これを追加
+import "./styles.css";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
